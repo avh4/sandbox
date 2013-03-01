fccbd2b5e8929b71c10bc5cbd7f89585b55d7b48
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-03-01 13:02:42 -0800

    Allow resources to be used at any path.

--- src/main/java/net/avh4/util/sandbox/Sandbox.java
+++ src/main/java/net/avh4/util/sandbox/Sandbox.java
@@ -1,61 +1,74 @@
 package net.avh4.util.sandbox;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-import org.apache.commons.io.FileUtils;
-
 public class Sandbox {
 
-	private final File root;
+    private final File root;
 
-	public Sandbox() {
-		try {
-			root = createTempDirectory();
-		} catch (final IOException e) {
-			throw new RuntimeException("Could not create a sandbox", e);
-		}
-	}
+    public Sandbox() {
+        try {
+            root = createTempDirectory();
+        } catch (final IOException e) {
+            throw new RuntimeException("Could not create a sandbox", e);
+        }
+    }
 
-	public File getRoot() {
-		return root;
-	}
+    public File getRoot() {
+        return root;
+    }
 
-	public File newFile(final String filename) {
-		return new File(getRoot(), filename);
-	}
+    public File newFile(final String filename) {
+        return new File(getRoot(), filename);
+    }
 
-	private static File createTempDirectory() throws IOException {
-		final File temp;
+    private static File createTempDirectory() throws IOException {
+        final File temp;
 
-		temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
+        temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
 
-		if (!(temp.delete())) {
-			throw new IOException("Could not delete temp file: "
-					+ temp.getAbsolutePath());
-		}
+        if (!(temp.delete())) {
+            throw new IOException("Could not delete temp file: "
+                    + temp.getAbsolutePath());
+        }
 
-		if (!(temp.mkdir())) {
-			throw new IOException("Could not create temp directory: "
-					+ temp.getAbsolutePath());
-		}
+        if (!(temp.mkdir())) {
+            throw new IOException("Could not create temp directory: "
+                    + temp.getAbsolutePath());
+        }
 
-		return (temp);
-	}
+        return (temp);
+    }
 
-	public void useResource(final String name) {
-		final File file = new File(root, name);
-		try {
-			final URL source = Sandbox.class.getResource("/" + name);
-			if (source == null) {
-				throw new RuntimeException("Unable to load resource '" + name
-						+ "'");
-			}
-			FileUtils.copyURLToFile(source, file);
-		} catch (final IOException e) {
-			throw new RuntimeException(
-					"Unable to load resource '" + name + "'", e);
-		}
-	}
+    public void useResource(final String resourceName) {
+        useResource(resourceName, resourceName);
+    }
+
+    public void useResource(String resourceName, String filePath) {
+        File file = new File(root, filePath);
+        useResource(resourceName, file);
+    }
+
+    private void useResource(String resourceName, File file) {
+        final URL source = getResourceUrl(resourceName);
+        try {
+            FileUtils.copyURLToFile(source, file);
+        } catch (final IOException e) {
+            throw new RuntimeException(
+                    "Unable to load resource '" + resourceName + "'", e);
+        }
+    }
+
+    private URL getResourceUrl(String resourceName) {
+        final URL source = Sandbox.class.getResource("/" + resourceName);
+        if (source == null) {
+            throw new RuntimeException("Unable to load resource '" + resourceName
+                    + "'");
+        }
+        return source;
+    }
 }
--- src/test/java/net/avh4/util/sandbox/SandboxTest.java
+++ src/test/java/net/avh4/util/sandbox/SandboxTest.java
@@ -1,85 +1,95 @@
 package net.avh4.util.sandbox;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.apache.commons.io.FileUtils;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
 public class SandboxTest {
 
-	@SuppressWarnings("rawtypes")
-	private static final Collection NO_FILES = new ArrayList<File>();
+    @SuppressWarnings("rawtypes")
+    private static final Collection NO_FILES = new ArrayList<File>();
 
-	@Test
-	public void testIsEmpty() {
-		final Sandbox emptySandbox = new Sandbox();
-		assertThat(emptySandbox.getRoot(), isEmptyDirectory());
-		assertEmptyDirectory(emptySandbox.getRoot());
-	}
+    @Test
+    public void testIsEmpty() {
+        final Sandbox emptySandbox = new Sandbox();
+        assertThat(emptySandbox.getRoot(), isEmptyDirectory());
+        assertEmptyDirectory(emptySandbox.getRoot());
+    }
 
-	@Test
-	public void testSandboxesAreIndependent() throws Exception {
-		final Sandbox s1 = new Sandbox();
-		final Sandbox s2 = new Sandbox();
-		final File f = s1.newFile("file.txt");
-		FileUtils.writeLines(f, Arrays.asList("test"));
-		assertThat(s1.getRoot(), not(isEmptyDirectory()));
-		assertThat(s2.getRoot(), isEmptyDirectory());
-	}
+    @Test
+    public void testSandboxesAreIndependent() throws Exception {
+        final Sandbox s1 = new Sandbox();
+        final Sandbox s2 = new Sandbox();
+        final File f = s1.newFile("file.txt");
+        FileUtils.writeLines(f, Arrays.asList("test"));
+        assertThat(s1.getRoot(), not(isEmptyDirectory()));
+        assertThat(s2.getRoot(), isEmptyDirectory());
+    }
 
-	@Test
-	public void testCreateFilesFromResources() throws Exception {
-		final Sandbox s = new Sandbox();
-		s.useResource("file_in_jar.txt");
+    @Test
+    public void testCreateFilesFromResources() throws Exception {
+        final Sandbox s = new Sandbox();
+        s.useResource("file_in_jar.txt");
 
-		final String fileContents = FileUtils.readFileToString(s
-				.newFile("file_in_jar.txt"));
-		assertThat(fileContents, is("MAGIC NUMBER 3849"));
-	}
+        final String fileContents = FileUtils.readFileToString(s
+                .newFile("file_in_jar.txt"));
+        assertThat(fileContents, is("MAGIC NUMBER 3849"));
+    }
 
-	@Test
-	public void testCorrectErrorMessageWhenResourceDoesntExist() {
-		try {
-			final Sandbox s = new Sandbox();
-			s.useResource("doesnt_exist.txt");
-			fail("Expected RuntimeException");
-		} catch (final NullPointerException e) {
-			fail("Should not throw NullPointerException");
-		} catch (final RuntimeException e) {
-			// pass
-		}
-	}
+    @Test
+    public void testCreateFilesFromResourcesInSpecificPaths() throws Exception {
+        final Sandbox s = new Sandbox();
+        s.useResource("file_in_jar.txt", "src/resources/file.txt");
 
-	private Matcher<? super File> isEmptyDirectory() {
-		return new TypeSafeMatcher<File>() {
+        final String fileContents = FileUtils.readFileToString(s.
+                newFile("src/resources/file.txt"));
+        assertThat(fileContents, is("MAGIC NUMBER 3849"));
+    }
 
-			@Override
-			public void describeTo(final Description description) {
-				description.appendText("an empty directory");
-			}
+    @Test
+    public void testCorrectErrorMessageWhenResourceDoesntExist() {
+        try {
+            final Sandbox s = new Sandbox();
+            s.useResource("doesnt_exist.txt");
+            fail("Expected RuntimeException");
+        } catch (final NullPointerException e) {
+            fail("Should not throw NullPointerException");
+        } catch (final RuntimeException e) {
+            // pass
+        }
+    }
 
-			@Override
-			protected boolean matchesSafely(final File item) {
-				@SuppressWarnings("rawtypes")
-				final Collection files = FileUtils.listFiles(item, null, true);
-				return files.equals(NO_FILES);
-			}
-		};
-	}
+    private Matcher<? super File> isEmptyDirectory() {
+        return new TypeSafeMatcher<File>() {
 
-	private static void assertEmptyDirectory(final File dir) {
-		assertThat(FileUtils.listFiles(dir, null, true), is(NO_FILES));
-	}
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("an empty directory");
+            }
+
+            @Override
+            protected boolean matchesSafely(final File item) {
+                @SuppressWarnings("rawtypes")
+                final Collection files = FileUtils.listFiles(item, null, true);
+                return files.equals(NO_FILES);
+            }
+        };
+    }
+
+    private static void assertEmptyDirectory(final File dir) {
+        assertThat(FileUtils.listFiles(dir, null, true), is(NO_FILES));
+    }
 
 }
